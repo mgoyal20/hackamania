74b0f5d27447fee8135d7c48aaf715e4255ef357
Shyam Pandya <shyam@dabeli>
2017-03-23 04:55:41 +0530

    YO

--- Downloads/hackamania-master/app.py
+++ Downloads/hackamania-master/app.py
@@ -48,8 +48,19 @@ def home():
     return render_template('pages/placeholder.home.html')
 
 
-@app.route('/about')
+@app.route('/about',methods=['GET','POST'])
 def about():
+    data = json.dumps({"username": session['username'],"requestCode": "GET_POST"}).encode('utf-8')
+    u = urllib2.Request("http://10.20.3.111:6000/", data, {"Content-Type": "application/json"})
+    f = urllib2.urlopen(u)
+    response = f.read()
+    f.close()
+    print data
+    print u
+    print response
+    res = json.loads(response)
+    for i in res['POSTS']:
+        print i['Category']
     return render_template('pages/placeholder.about.html')
 
 
@@ -68,7 +79,12 @@ def login():
         print data
         print u
         print response
+        res=json.loads(response)
         session['username']=user
+        if res['SERVER_RESPONSE']=='1':
+            print "Successful"
+        else:
+            print "Unsuccessful"
     return render_template('forms/login.html', form=forms)
 
 @app.route('/register',methods=['GET','POST'])
@@ -101,7 +117,7 @@ def register():
 def post():
     form = PostForm(request.form)
     if request.method == "POST":
-        user="a"
+        user=session['username']
         phone="b"
         email="c"
         address="d"
